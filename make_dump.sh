85017f074dc248e5f004ce3f275e8278d3738a9e
anastasiia.fedorova <anastasiia.fedorova@simbuka.com>
2022-08-29 16:52:41 +0200

    write logs properly and add scripts

---
+++ make_dump.sh
@@ -0,0 +1,1 @@
+docker exec -i incremental_backups bash -c 'mysqldump -p$MYSQL_ROOT_PASSWORD --skip-comments --flush-logs --delete-master-logs --single-transaction --all-databases | gzip' > ./$(date +%d-%m-%Y_%H-%M-%S)-inc.gz
