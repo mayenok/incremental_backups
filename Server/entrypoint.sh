7e6d840d08dc59478f83cf92b3aebbfd67d0591f
anastasiia.fedorova <anastasiia.fedorova@simbuka.com>
2022-08-30 16:01:06 +0200

    add log files to database

--- Server/entrypoint.sh
+++ Server/entrypoint.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-#source docker-entrypoint.sh mysqld
-
 bash /usr/local/bin/docker-entrypoint.sh mysqld --help
 
 rm -- /var/log/mysql/* 2>/dev/null || true
 
+shopt -s extglob
+
 _term() {
+  echo "Saving SQL logs"
+  mysqlbinlog --skip-annotate-row-events \
+              --base64-output=never \
+              --short-form \
+              /var/log/mysql/mysql-bin.+([0-9]) \
+              > /volumes/incremental-logs/"$(date +%Y-%m-%d_%H-%M-%S)".sql
+
+  echo "Adding log entries into a database"
+  for entry in "/volumes/incremental-logs"/*
+  do
+   filename=$(basename "$entry")
+   mysql -p"$MYSQL_ROOT_PASSWORD" -e "use test_db;insert ignore into executed_incremental_logs (log) values ('$filename');"
+  done
+
   echo "Stopping mysql server"
   MYSQL_PWD="$MYSQL_ROOT_PASSWORD" mysqladmin shutdown -uroot
 }
@@ -18,10 +32,3 @@ bash /usr/local/bin/docker-entrypoint.sh mysqld &
 
 child=$!
 wait "$child"
-
-shopt -s extglob
-mysqlbinlog --skip-annotate-row-events \
-            --base64-output=never \
-            --short-form \
-            /var/log/mysql/mysql-bin.+([0-9]) \
-            > /volumes/incremental-logs/$(date +%Y-%m-%d_%H-%M-%S).sql
